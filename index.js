8a17796e164c118d92554a6a0dd3f605b000fb81
Amarnath Rajasekaran <amarnath-r@HCL.COM>
2019-08-06 14:21:15 +0530

    superagent promise

--- index.js
+++ index.js
@@ -2,19 +2,23 @@ const fs = require('fs');
 //const http = require('http');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`,(err,data)=>{
-   // console.log(`Breed: ${data}`)
-   superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then()
-   .end((err,res) =>{
-       //https://dog.ceo/api/breed/hound/images
-        if(err)
-        {
-           return console.log(err.message)
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+    // console.log(`Breed: ${data}`)
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res => {
+
+        fs.writeFile('dog-img.txt', res.body.message, err => {
+            console.log('random dog image saved to the file');
+        })
+    }).catch(err => {
+        if (err) {
+            return console.log(err.message)
         }
-   console.log(res.body.message);
-    fs.writeFile('dog-img.txt', res.body.message, err =>{
-        console.log('random dog image saved to the file');
     })
-   })
+    // .end((err,res) =>{
+    //https://dog.ceo/api/breed/hound/images
+
+    //console.log(res.body.message);
+
 })
+
 
