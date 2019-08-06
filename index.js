dcbac099c04fac0f3360fa8edf6c2422add5d404
Amarnath <sharewithamar@gmail.com>
2019-08-06 09:59:29 -0700

    Multiple Promise - Promise All

--- index.js
+++ index.js
@@ -23,11 +23,19 @@ const getDogPic = async () => {
   try {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed:${data}`);
-    const res = await superagent.get(
+    const res1Pro = await superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
-    await writeFilePro('dog-img.txt', res.body.message);
+    const res2Pro = await superagent.get(
+        `https://dog.ceo/api/breed/${data}/images/random`
+      );
+      const res3Pro = await superagent.get(
+        `https://dog.ceo/api/breed/${data}/images/random`
+      );
+      const all = await Promise.all([res1Pro,res2Pro,res3Pro])
+      const imgs = all.map(item => item.body.message);
+    console.log(imgs);
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log(`Random dog image saved to file`);
   } catch (err) {
       
